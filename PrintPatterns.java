40f2ecf652afdd14709a43308f40082e20c1179b
Lakee Rawat <73211332+lakeerawat@users.noreply.github.com>
2023-05-01 15:44:03 +0530

    Add files via upload

---
+++ PrintPatterns.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class PrintPatterns {
+    public static void main(String[] args) {
+
+//        pattern5(5);
+        pattern(120);
+    }
+    static void pattern(int n){
+        for(int row=1; row<=2*n; row++){
+            int totalColInRows = row>n ? 2*n-row : row;
+
+            for(int s=0;s<n-totalColInRows; s++){
+                System.out.print(" ");
+            }
+            for(int col=1; col<=totalColInRows; col++){
+                System.out.print((col*2)-1);
+            }
+            for(int col=2; col<=totalColInRows; col++){
+                int letter=65;
+                System.out.print((char)(letter+col-2));
+            }
+            System.out.println();
+        }
+    }
+}
